c812fcd3b9dd6d707f68bbc7ea21077933ec7f97
Veronica Kim <vero.a.kim@gmail.com>
2017-12-21 16:05:02 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,7 +2,8 @@ def turn(board)
   puts "Please enter 1-9:"
   if input_to_index(input)? == true
     valid_move?(board, index)
-  else 
+  else
+    "Please enter 1-9:
 end
 
 def display_board(board)
